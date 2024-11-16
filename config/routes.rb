e60729772985d5442a5c86c8222441ca75f4bf37
Nitish Kumar Choudhary <nitishchoudhary688@gmail.com>
2024-11-16 07:10:14 +0530

    implemeted login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :hospital_staffs
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # namespace :landing_page do
-    root "landing_page/homes#index"
-  # end
+  root "landing_page/homes#index"
+
+  devise_for :hospital_staffs
+
 end
